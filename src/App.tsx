eb0e87ec8d2aca197ffa11410adc84029278a484
Kostiantyn Shestakov <kostiashestakov10@gmail.com>
2024-12-23 18:42:16 +0200

    phone-catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Header } from './modules/shared/Header';
+import { Footer } from './modules/shared/Footer';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="App">
+      <Header />
+
+      <main className="App__page page">
+        <Outlet />
+      </main>
+
+      <Footer />
     </div>
   );
 };
